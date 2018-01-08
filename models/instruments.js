2a8f9f09d2f48c49be68b4fe9f4f40a279630af1
Lillian Chernin <l.smith1654@gmail.com>
2018-01-08 13:49:12 -0600

    create general folder structure and instruments array

---
+++ models/instruments.js
@@ -0,0 +1,40 @@
+const instruments = [
+  {
+    id: 1,
+    name: 'violin',
+    family: 'string',
+    image: '/static/img/violin.jpeg'
+  },
+  {
+    id: 2,
+    name: 'cello',
+    family: 'string',
+    image: '/static/img/cello.jpeg'
+  },
+  {
+    id: 3,
+    name: 'guitar',
+    family: 'string',
+    image: '/static/img/guitar.jpeg'
+  },
+  {
+    id: 4,
+    name: 'trumpet',
+    family: 'brass',
+    image: '/static/img/trumpet.jpeg'
+  },
+  {
+    id: 5,
+    name: 'french horn',
+    family: 'brass',
+    image: '/static/img/french horn.jpeg'
+  },
+  {
+    id: 6,
+    name: 'piano',
+    family: 'percussion',
+    image: '/static/img/piano.jpeg'
+  }
+];
+
+module.exports = instruments;
